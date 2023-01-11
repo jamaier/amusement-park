d4b3b9cb77f05edce91e922e63ec42d0939f18b9
Jacob Maier <jacobamaier@gmail.com>
2023-01-11 14:20:37 -0800

    Add conditionals for rides.
    Co-authored-by: Eliot Gronstal <eliot.lauren@gmail.com>

--- js/script.js
+++ js/script.js
@@ -13,5 +13,21 @@ function hideResults() {
       hideResults();
       const age = parseInt(document.querySelector("input#age").value);
       const height = parseInt(document.querySelector("input#height").value);
-    };
+      
+      if (age >= 12 && height >= 60) {
+        document.getElementById("swings").removeAttribute("class");
+        document.getElementById("coaster").removeAttribute("class");
+        document.getElementById("tower").removeAttribute("class");
+        }
+        else if (age >= 12 || height >= 48) {
+        document.getElementById("swings").removeAttribute("class");
+        document.getElementById("coaster").removeAttribute("class");
+        }
+        else if (age >= 6) {
+          document.getElementById("swings").removeAttribute("class");
+        }
+        else {
+          document.getElementById("sorry").removeAttribute("class");
+        }
+      };
   };
